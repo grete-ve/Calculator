61d46cf48b3cd97ef445ac5833b16665e6736b67
Grete <greteveetla@gmail.com>
2024-01-12 22:53:49 +0200

    Fixed numbers and operators splitting, and two digit numbers (float) work

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 from calculator import Calculator
+import re
 
 # this is main function
 def main():
     # create instance of Calculator class
     calc = Calculator()
     # ask user for input and remove spaces from the input string
-    input_str = input("Enter the expression: ").replace(" ", "")
-    
+    #input_str = input("Enter the expression: ").replace(" ", "")
+    input_str = '0-10-10'
     # split the numbers and operators
-    numbers = [int(input_str[i]) for i in range(0, len(input_str), 2)]
-    operators = [input_str[i] for i in range(1, len(input_str), 2)]
+    numbers = []
+    for t in re.split(r'(\d*\.?\d*)', input_str):
+        try:
+            numbers.append(float(t))
+        except ValueError:
+            pass
+
+    all_operators = ['+', '-', '*', '/']
+    operators = []
+    for s in input_str:
+        if (s in all_operators):
+            operators.append(s)
     
     # check if '*' or '/' is present in operators
     if '*' in operators or '/' in operators:
@@ -22,4 +33,5 @@ def main():
 
 
 # call main function
-main()
+if __name__ == "__main__":
+    main()
